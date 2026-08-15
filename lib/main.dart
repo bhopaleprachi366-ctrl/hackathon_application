175f09dc933504074c414746529e7bc24ff5e311
Prachi <bhopaleprachi366@gmail.com>
2026-08-15 13:21:27 +0530

    main .dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,31 @@
-
 import 'package:flutter/material.dart';
-import 'package:hackathon_application/customer_side/homepage.dart';
-import 'package:hackathon_application/navigation/bottom_navigation.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'package:hackathon_application/navigation/bottom_navigation.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const SubServeApp());
 }
 
 class SubServeApp extends StatelessWidget {
   const SubServeApp({super.key});
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:hackathon_application/screens/login_page.dart';
-import 'firebase_options.dart';
-
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const MyApp());
-}
-
-class MyApp extends void StatelessWidget {
-  const MyApp({super.key});
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       title: 'SubServe',
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'Poppins',
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1565C0)),
       ),
-      home: NavigationPage(),
+      home: const NavigationPage(),
     );
   }
-}
 }
